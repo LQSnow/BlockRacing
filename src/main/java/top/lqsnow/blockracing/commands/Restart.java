ccc4a5c53a218e1cc929b0d38338846ac4fe3e0c
LQSnow <lq_snow@outlook.com>
2023-09-18 13:22:05 +0800

    修复Bug，添加Debug命令

--- src/main/java/top/lqsnow/blockracing/commands/Restart.java
+++ src/main/java/top/lqsnow/blockracing/commands/Restart.java
@@ -11,7 +11,6 @@ import org.jetbrains.annotations.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
-import static top.lqsnow.blockracing.utils.ColorUtil.t;
 import static top.lqsnow.blockracing.utils.ConsoleCommandHandler.sendAll;
 
 public class Restart implements CommandExecutor {
