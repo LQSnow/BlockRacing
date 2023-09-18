b98a51b9b4295d1c2a4905617dfc239c15af29f1
LQSnow <lq_snow@outlook.com>
2023-09-18 13:20:27 +0800

    修复Bug，添加Debug命令

--- src/main/java/top/lqsnow/blockracing/commands/Restart.java
+++ src/main/java/top/lqsnow/blockracing/commands/Restart.java
@@ -7,7 +7,6 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
-import top.lqsnow.blockracing.utils.ConsoleCommandHandler;
 
 import java.util.ArrayList;
 import java.util.List;
