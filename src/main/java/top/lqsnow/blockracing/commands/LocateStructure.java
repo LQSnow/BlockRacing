2aece434e64963ba50c55950ad4059981ff5d3ad
Administrator <xiaojiuwo233@gmail.com>
2024-10-04 15:56:23 +0800

    update: 1.21 locateStructure & lang

--- src/main/java/top/lqsnow/blockracing/commands/LocateStructure.java
+++ src/main/java/top/lqsnow/blockracing/commands/LocateStructure.java
@@ -36,6 +36,6 @@ public class LocateStructure implements CommandExecutor, TabCompleter {
     @Nullable
     @Override
     public List<String> onTabComplete(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
-        return Arrays.asList("ancient_city", "buried_treasure", "end_city", "fortress", "mansion", "mineshaft", "mineshaft_mesa", "monument", "ocean_ruin_cold", "ocean_ruin_warm", "shipwreck", "shipwreck_beached", "stronghold", "desert_pyramid", "igloo", "jungle_pyramid", "swamp_hut", "village_desert", "village_plains", "village_savanna", "village_snowy", "village_taiga", "pillager_outpost", "nether_fossil", "bastion_remnant", "ruined_portal", "ruined_portal_desert", "ruined_portal_jungle", "ruined_portal_mountain", "ruined_portal_ocean", "ruined_portal_swamp", "ruined_portal_nether");
+        return Arrays.asList("ancient_city", "buried_treasure", "end_city", "fortress", "mansion", "mineshaft", "mineshaft_mesa", "monument", "ocean_ruin_cold", "ocean_ruin_warm", "shipwreck", "shipwreck_beached", "stronghold", "desert_pyramid", "igloo", "jungle_pyramid", "swamp_hut", "village_desert", "village_plains", "village_savanna", "village_snowy", "village_taiga", "pillager_outpost", "nether_fossil", "bastion_remnant", "ruined_portal", "ruined_portal_desert", "ruined_portal_jungle", "ruined_portal_mountain", "ruined_portal_ocean", "ruined_portal_swamp", "ruined_portal_nether", "trial_chambers");
     }
 }
